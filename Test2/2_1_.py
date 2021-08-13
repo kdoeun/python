8469614d63480fe7f00187a0e839f9e23bb883ae
khjyo2001 <khjyo2001@gmail.com>
2021-08-13 17:21:39 +0900

    2021/08/13 Test2 완료

---
+++ Test2/2_1_.py
@@ -0,0 +1,28 @@
+"""
+날짜 : 2021/0813
+이름 : 김도은
+내용 : 파이썬 리스트에 점수를 입력하고 리스트 총합을 구하기
+"""
+
+scores = []
+
+while True:
+    print('0: 종료, 1:입력')
+    result = input('입력 :')
+
+    if result == '0':
+        break
+
+    score = input(('점수입력 :'))
+
+    scores.append(score)
+
+print('입력한 점수 확인')
+print(scores)
+
+sum = 0
+
+for score in scores:
+    sum += score
+
+print('리스트 총합 :', sum)
